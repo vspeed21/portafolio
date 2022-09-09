041e9e61d44a7573794e63c927a92370a244dd42
vspeed21 <vtorresquintanilla0206@gmail.com>
2022-09-09 09:47:56 -0600

    react-icons agregado

--- src/components/Contacto.js
+++ src/components/Contacto.js
@@ -1,5 +1,5 @@
-import 'boxicons'
-
+import { GoMarkGithub, GoMail } from "react-icons/go";
+import { BsWhatsapp } from "react-icons/bs";
 import styles from '../../styles/Contacto.module.css'
 import Formulario from './Formulario'
 
@@ -14,19 +14,27 @@ const Contacto = () => {
           </h3>
 
           <div className={styles.email}>
-            <a href='mailito:vtorresquintanilla0206@gmail.com'>
-              <box-icon name='gmail' type='logo' size='cssSize'></box-icon>
+            <a 
+              href='mailito:vtorresquintanilla0206@gmail.com'
+              target={'_blank'}
+              rel="noopener noreferrer"
+            >
+              <GoMail />
             </a>
             <p>vtorresquintanilla0206@gmail.com</p>
           </div>
 
           <div className={styles.flex}>
-            <a href='https://api.whatsapp.com/send?phone=50499152520'>
-              <box-icon name='whatsapp' type='logo' size='cssSize'></box-icon>
+            <a 
+              href='https://api.whatsapp.com/send?phone=50499152520'
+              target={'_blank'}
+              rel="noopener noreferrer"
+            >
+              <BsWhatsapp />
             </a>
 
             <a href='https://github.com/vspeed21'>
-              <box-icon name='github' type='logo' size='cssSize'></box-icon>
+              <GoMarkGithub />
             </a>
           </div>
         </div>
