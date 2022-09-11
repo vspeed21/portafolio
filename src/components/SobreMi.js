96b2562e455cd5b4f3ff7f184f244d837b21f88a
vspeed21 <vtorresquintanilla0206@gmail.com>
2022-09-11 09:27:19 -0600

    animaciones key quitadas

--- src/components/SobreMi.js
+++ src/components/SobreMi.js
@@ -4,26 +4,6 @@ import Link from 'next/link'
 import styles from '../../styles/SobreMi.module.css';
 
 const SobreMi = () => {
-  const [animacion, setAnimacion] = useState(false);
-  const divRef = useRef();
-
-  useEffect(() => {
-    const handleScroll = () => {
-      const div = divRef.current;
-      const { y } = div.getBoundingClientRect();
-
-      // console.log(y);
-      
-      y <= 270 ? setAnimacion(true) : setAnimacion(false);
-    }
-
-    window.addEventListener('scroll', handleScroll);
-  
-    return () => {
-      window.removeEventListener('scroll', handleScroll);
-    }
-  }, [])
-  
 
   return (
     <div className='contenedor'>
@@ -31,7 +11,7 @@ const SobreMi = () => {
 
       <div ref={divRef} className={styles.sobre}>
       
-        <div className={`${animacion ? 'animacionTexto' : ''} ${styles.texto}`}>
+        <div className={styles.texto}>
           <p>
             Soy una persona responsable, honesta, apasionado en mi área. He realizado diversos sitios web con diferentes tecnologías que me han permitido practicar mis conocimientos y así obtener cierto nivel de experiencia.
           </p>
@@ -47,7 +27,7 @@ const SobreMi = () => {
           </Link>
         </div>
 
-        <div className={`${styles.imagenes} ${animacion ? 'animacionImagen' : ''}`}>
+        <div className={styles.imagenes}>
           <img
             src={'/img/sobremi-1.jpg'}
             alt={'imagen sobre-mi seccion'}
--- src/components/Tabla.js
+++ src/components/Tabla.js
@@ -1,37 +1,16 @@
-import { useState, useEffect, useRef } from 'react';
-
 import styles from '../../styles/Tabla.module.css';
 
 const Tabla = () => {
-  const [animacion, setAnimacion] = useState(false);
-  const divRef = useRef();
-
-  useEffect(() => {
-    const handleScroll = () => {
-      const div = divRef.current;
-      const { y } = div.getBoundingClientRect();
-
-      y <= 230 ? setAnimacion(true) : setAnimacion(false);
-
-    }
-
-    window.addEventListener('scroll', handleScroll);
-
-    return () => {
-      window.removeEventListener('scroll', handleScroll);
-    }
-  }, [])
-  
 
   return (
     <div className={styles.tabla}>
-      <div ref={divRef} className={`${styles.flex} contenedor`}>
-        <div className={`${styles.categoria} ${animacion ? 'animacionProyectos' : ''}`}>
+      <div className={`${styles.flex} contenedor`}>
+        <div className={styles.categoria}>
           <p>40+</p>
           <p>Proyectos</p>
         </div>
 
-        <div className={`${styles.categoria} ${animacion ? 'animacionXP' : ''}`}>
+        <div className={styles.categoria}>
           <p>2+</p>
           <p>Años de experiencia</p>
         </div>
