6e76619b4412ad4f0d1ac1f69c538cc6b2ca1930
vspeed21 <vtorresquintanilla0206@gmail.com>
2022-09-16 09:44:27 -0600

    disponibilidad agregada al footer

--- src/layout/Footer.js
+++ src/layout/Footer.js
@@ -2,34 +2,45 @@ import Link from 'next/link'
 import styles from '../../styles/Footer.module.css'
 
 const Footer = () => {
-    const year = new Date().getFullYear();
+  const year = new Date().getFullYear();
 
   return (
     <footer className={styles.footer}>
-        <div className={`${styles.flex} contenedor`}>
-            <nav className={styles.navegacion}>
-                <Link href='/skills'>
-                    Habilidades
-                </Link>
+      <div className={`${styles.flex} contenedor`}>
+        <div>
+          <nav className={styles.navegacion}>
+            <Link href='/skills'>
+              Habilidades
+            </Link>
 
-                <Link href='/contacto'>
-                    Contacto
-                </Link>
-            </nav>
+            <Link href='/contacto'>
+              Contacto
+            </Link>
+          </nav>
 
-            <div className={styles.copyright}>
-                <p>Todos los derechos reservados &#169; <span>{year}</span> </p>
-                <p>Hecho por: {''}
-                    <a 
-                        href='https://www.github.com/vspeed21' 
-                        target="_blank"
-                        rel="noopener noreferrer"
-                    >
-                        Victor Torres
-                    </a>
-                </p>
+          <div className={styles.disponibilidad}>
+            <p>Estado:</p>
+            
+            <div>
+              <span></span>
+              <p>Disponible</p>
             </div>
+          </div>
         </div>
+
+        <div className={styles.copyright}>
+          <p>Todos los derechos reservados &#169; <span>{year}</span> </p>
+          <p>Hecho por: {''}
+            <a
+              href='https://www.github.com/vspeed21'
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Victor Torres
+            </a>
+          </p>
+        </div>
+      </div>
     </footer>
   )
 }
