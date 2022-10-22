330dab319a3f4bfd2343146bd002f412dd64f620
vspeed21 <vtorresquintanilla0206@gmail.com>
2022-10-21 22:05:28 -0600

    pages/skills: funcion para fetch de strapi

--- pages/skills.js
+++ pages/skills.js
@@ -2,24 +2,48 @@ import Layout from "../src/layout/Layout"
 import ListadoSkills from "../src/components/ListadoSkills";
 import styles from '../styles/Listado.module.css'
 
-import { Frontend, Backend, Herramientas } from '../src/data/skills'
-
-const skills = () => {
+const skills = ({ frontend, backend, herramientas }) => {
 
   return (
     <Layout pagina='Habilidades'>
-      <div className={`${styles.texto}`}>
+      <div className={styles.texto}>
         <h2 className='heading'>Mis Habilidades</h2>
         <p>A continuacion se presentan mis habilidades en el desarrollo web; tanto para el lado del cliente como el lado del servidor</p>
       </div>
 
       <ListadoSkills 
-        frontends={Frontend}
-        backends={Backend}
-        herramientas={Herramientas}
+        frontends={frontend.data}
+        backends={backend.data}
+        herramientas={herramientas.data}
       />
     </Layout>
   )
+}
+
+export async function getServerSideProps() {
+  const urlFrontend = `${process.env.API_URL}/api/frontends?populate=imagen`;
+  const urlBackend = `${process.env.API_URL}/api/backends?populate=imagen`;
+  const urlHerramientas = `${process.env.API_URL}/api/herramientas?populate=imagen`;
+
+  const [ resFrontend, resBackend, resHerramientas ] = await Promise.all([
+    fetch(urlFrontend),
+    fetch(urlBackend),
+    fetch(urlHerramientas),
+  ]);
+
+  const [ frontend, backend, herramientas ] = await Promise.all([
+    resFrontend.json(),
+    resBackend.json(),
+    resHerramientas.json()
+  ]);
+
+  return{
+    props:{
+      frontend,
+      backend,
+      herramientas,
+    }
+  }
 }
 
 export default skills
