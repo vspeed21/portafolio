3c896314fe00b5af210c84c74b12849b27476ea1
Victor Torres <vtorresquintanilla0206@gmail.com>
2022-12-16 14:19:36 -0600

    dark class to body added

--- src/layout/Layout.js
+++ src/layout/Layout.js
@@ -14,9 +14,11 @@ const Layout = ({children, pagina, informacion}) => {
         <meta name="keywords" content="portafolio web, proyectos web, skills" />
       </Head>
 
+      <body className="bg-body-dark h-screen">
         <Header informacion={informacion} />
           {children}
         <Footer />
+      </body>
 
     </>
   )
