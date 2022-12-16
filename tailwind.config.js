9739119d29457408938cfd43772e2c57713733fe
Victor Torres <vtorresquintanilla0206@gmail.com>
2022-12-16 14:20:05 -0600

    components created for header on tailwind config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -24,7 +24,17 @@ module.exports = {
           margin: '0 auto'
         },
         '.hero': {
-          backgroundImage: "linear-gradient(to right, rgb(0 0 0 /.7) 50%, rgb(0 0 0 /.9) 100%), url('/img/header.png')"
+          backgroundImage: "linear-gradient(to right, rgb(0 0 0 /.7) 50%, rgb(0 0 0 /.9) 100%), url('/img/header.png')",
+
+          "&.wrapper": {
+            height: '90vh'
+          },
+          '.gradient': {
+            background: 'linear-gradient(to right, #F16529 33%, #2465F1 34%, #2465F1 66%, #F7DF1E 67%, #F7DF1E 100%)',
+            fontSize: '16rem',
+            '-webkit-background-clip': 'text',
+            backgroundClip: 'text',
+          }
         }
       });
     })
