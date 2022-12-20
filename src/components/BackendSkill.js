fec1a4aff7ce0de9f6935d80b88315a17da5f5fe
Victor Torres <vtorresquintanilla0206@gmail.com>
2022-12-20 16:04:04 -0600

    backend skill created

--- src/components/BackendSkill.js
+++ src/components/BackendSkill.js
@@ -1,25 +1,35 @@
-import Image from 'next/image';
+import Image from 'next/image'
 
-const BackendSkill = ({skill}) => {
-    const { nombre, descripcion, imagen} = skill.attributes;
+import nodeIcon from '../../public/icons/nodejs-color.svg'; 
+import npmIcon from '../../public/icons/npm-color.svg';
+import dbIcon from '../../public/icons/database.svg'
 
-  return (
-    <div className={styles.skill}>
-        <Image 
-            width={300}
-            height={250}
-            src={imagen.data.attributes.url}
-            alt={`imagen tecnologia ${nombre}`}
-        />
+ const BackendSkill = () => {
+	
+  const images = [nodeIcon, npmIcon, dbIcon]
 
-        <div className={styles.textos}>
-            <p className={styles.nombre}>{nombre}</p>
-            {descripcion && 
-                <p className={styles.descripcion}>{descripcion}</p>
-            }
-        </div>
-    </div>
-  )
+	return (
+		<>
+			<ul className='list-disc md:text-lg ml-5 md:col-start-2 md:col-end-3'>
+				<h2 className='text-white-dark text-2xl font-bold text-center'>Back End</h2>
+				<li className="mt-5">REST APIs with Node.js & express</li>
+				<li>NPM for package management</li>
+				<li>Mongo DB - mongoose</li>
+				<li>React</li>
+			</ul>
+
+			<div className='grid grid-cols-2 items-center sm:flex justify-evenly gap-4 bg-gray-500 md:col-start-1 md:col-end-2 md:row-start-2 md:row-end-3 py-3 md:py-0'>
+				{images.map(image => (
+					<Image
+						src={image}
+						alt="icon"
+						width={50}
+						height={50}
+					/>
+				))}
+			</div>
+		</>
+	)
 }
 
 export default BackendSkill
--- src/components/SkillSection.js
+++ src/components/SkillSection.js
@@ -1,18 +1,14 @@
-import reactIcon from '../../public/icons/react-color.svg'; 
-import cssIcon from '../../public/icons/css-color.svg';
-import sassIcon from '../../public/icons/sass-color.svg'
-import tailwindIcon from '../../public/icons/tailwindcss-color.svg';
+
 import FrontendSkill from './FrontendSkill';
 import BackendSkill from './BackendSkill';
 
 function SkillSection() {
-	
-  const images = [reactIcon, cssIcon, sassIcon, tailwindIcon]
 
 	return (
 		<section>
 			<div className="grid md:grid-cols-2 md:grid-rows-3 gap-5 text-white max-w-3xl mx-auto p-4 border-4 border-gray-600 rounded-md">
 				<FrontendSkill/>
+				<BackendSkill/>
 			</div>
 		</section>
 	)
