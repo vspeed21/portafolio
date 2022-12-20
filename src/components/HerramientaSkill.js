55a8fb1062769550698e6fe382db5c5698a8398e
Victor Torres <vtorresquintanilla0206@gmail.com>
2022-12-20 16:22:29 -0600

    tools skill done

--- src/components/HerramientaSkill.js
+++ src/components/HerramientaSkill.js
@@ -1,25 +1,31 @@
 import Image from 'next/image';
 
-const BackendSkill = ({herramienta}) => {
-    const { nombre, descripcion, imagen } = herramienta.attributes;
+import gitIcon from '../../public/icons/git-color.svg'; 
+import githubIcon from '../../public/icons/github-color.svg';
+
+const HerramientaSkill = () => {
+	const images = [gitIcon, githubIcon]
 
   return (
-    <div className="">
-        <Image 
-            width={300}
-            height={nombre === 'Gulp' ? 400 : 250}
-            src={imagen.data.attributes.url}
-            alt={`imagen tecnologia ${nombre}`}
-        />
+		<>
+			<ul className='list-disc md:text-lg ml-5'>
+				<h2 className='text-white-dark text-2xl font-bold text-center'>Tools</h2>
+				<li className="mt-5">Git for version control</li>
+				<li>GitHub to collaborate and open source projects</li>
+			</ul>
 
-        <div className="">
-            <p className="">{nombre}</p>
-            {descripcion && 
-                <p className="">{descripcion}</p>
-            }
-        </div>
-    </div>
-  )
+			<div className='grid grid-cols-2 items-center sm:flex justify-evenly gap-4 bg-gray-500 py-3 md:py-0'>
+				{images.map(image => (
+					<Image
+						src={image}
+						alt="icon"
+						width={50}
+						height={50}
+					/>
+				))}
+			</div>
+		</>
+	)
 }
 
-export default BackendSkill
+export default HerramientaSkill
--- src/components/SkillSection.js
+++ src/components/SkillSection.js
@@ -1,6 +1,7 @@
 
 import FrontendSkill from './FrontendSkill';
 import BackendSkill from './BackendSkill';
+import HerramientaSkill from './HerramientaSkill';
 
 function SkillSection() {
 
@@ -9,6 +10,7 @@ function SkillSection() {
 			<div className="grid md:grid-cols-2 md:grid-rows-3 gap-5 text-white max-w-3xl mx-auto p-4 border-4 border-gray-600 rounded-md">
 				<FrontendSkill/>
 				<BackendSkill/>
+				<HerramientaSkill/>
 			</div>
 		</section>
 	)
