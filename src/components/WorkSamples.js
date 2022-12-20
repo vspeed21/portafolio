59a5e4b4521671e92753b08e4c6340058ad92b5e
Victor Torres <vtorresquintanilla0206@gmail.com>
2022-12-19 21:59:47 -0600

    show button and redirect to user to see more projects

--- src/components/WorkSamples.js
+++ src/components/WorkSamples.js
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import Project from "./Project";
 
 function WorkSamples({ projects }) {
@@ -11,13 +12,25 @@ function WorkSamples({ projects }) {
       <p className="text-white text-center max-w-lg mx-auto">In my experience I have developed several websites with different technologies, below I show you some of them</p>
 
       <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-5 xl:grid-cols-4">
-        {projects?.map( project => (
+        {projects.length < 5 ? projects?.map( project => (
           <Project
             key={project.id}
             project={project}
           />
-        ))}
+        )): null}
       </div>
+
+      {projects.length > 4 ? (
+        <div className="flex justify-end">
+        <Link href="/proyectos">
+          <a
+            className="bg-primary py-1 px-3 rounded text-white font-bold mt-6 hover:bg-secondary transition-colors duration-300"
+          >
+            See more
+          </a>
+        </Link>
+      </div>
+      ): null}
     </section>
   )
 }
