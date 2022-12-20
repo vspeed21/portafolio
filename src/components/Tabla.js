e4da6408bdcf1789b466b5f2da75d9204f52d947
Victor Torres <vtorresquintanilla0206@gmail.com>
2022-12-19 21:36:24 -0600

    fixing gap on Table

--- src/components/Tabla.js
+++ src/components/Tabla.js
@@ -3,19 +3,19 @@ const Tabla = () => {
 
   return (
     <section className="bg-secondary mt-16 py-12">
-      <div className=" contenedor flex flex-col md:flex-row justify-evenly items-center text-white">
+      <div className=" contenedor flex flex-col md:flex-row justify-evenly items-center text-white gap-9 md:gap-0">
         <div className="text-center">
-          <p className="text-6xl mb-2 font-bold">2+</p>
+          <p className="text-5xl md:text-6xl mb-2 font-bold">2+</p>
           <p className="text-xl first-letter:uppercase">years of experiencie</p>
         </div>
 
         <div className="text-center">
-          <p className="text-6xl mb-2 font-bold">50+</p>
+          <p className="text-5xl md:text-6xl mb-2 font-bold">50+</p>
           <p className="text-xl first-letter:uppercase">projects</p>
         </div>
 
         <div className="text-center">
-          <p className="text-6xl mb-2 font-bold">20+</p>
+          <p className="text-5xl md:text-6xl mb-2 font-bold">20+</p>
           <p className="text-xl first-letter:uppercase">skills</p>
         </div>
       </div>
