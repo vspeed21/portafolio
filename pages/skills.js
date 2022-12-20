6db2df1f97835ec98a05ea906fb7e7b0b5a76c07
Victor Torres <vtorresquintanilla0206@gmail.com>
2022-12-20 16:39:15 -0600

    skills page done

--- pages/skills.js
+++ pages/skills.js
@@ -1,43 +1,13 @@
 import Layout from "../src/layout/Layout"
 import ListadoSkills from "../src/components/ListadoSkills";
 
-const skills = ({ frontend, backend, herramientas }) => {
+const skills = () => {
 
   return (
-    <Layout pagina='Habilidades'>
-      <ListadoSkills 
-        frontends={frontend.data}
-        backends={backend.data}
-        herramientas={herramientas.data}
-      />
+    <Layout pagina='Skills'>
+      <ListadoSkills/>
     </Layout>
   )
-}
-
-export async function getServerSideProps() {
-  const urlFrontend = `${process.env.API_URL}/api/frontends?populate=imagen`;
-  const urlBackend = `${process.env.API_URL}/api/backends?populate=imagen`;
-  const urlHerramientas = `${process.env.API_URL}/api/herramientas?populate=imagen`;
-
-  const [ resFrontend, resBackend, resHerramientas ] = await Promise.all([
-    fetch(urlFrontend),
-    fetch(urlBackend),
-    fetch(urlHerramientas),
-  ]);
-
-  const [ frontend, backend, herramientas ] = await Promise.all([
-    resFrontend.json(),
-    resBackend.json(),
-    resHerramientas.json()
-  ]);
-
-  return{
-    props:{
-      frontend,
-      backend,
-      herramientas,
-    }
-  }
 }
 
 export default skills
