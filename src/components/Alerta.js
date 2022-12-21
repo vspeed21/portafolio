036380b061dafc2b8c0599b6a5abe73609faa012
Victor Torres <vtorresquintanilla0206@gmail.com>
2022-12-21 16:45:15 -0600

    tailwind classes added to Alerta component

--- src/components/Alerta.js
+++ src/components/Alerta.js
@@ -1,6 +1,6 @@
 const Alerta = ({children, error, msg}) => {
   return (
-    <p className={`${styles.alerta} ${error ? styles.error : styles.exito}`}>{children || msg}</p>
+    <p className={`text-center font-bold py-2 border-l-4 mt-2 ${error ? 'bg-red-100 border-l-red-400 text-red-700': 'bg-green-100 border-l-green-400 text-green-700'}`}>{children || msg}</p>
   )
 }
 
