103b1cced7e0b62b62e67c5bddf127f4a264d26d
Victor Torres <vtorresquintanilla0206@gmail.com>
2022-12-26 13:52:53 -0600

    fetching skills from strapi

--- pages/skills.js
+++ pages/skills.js
@@ -1,13 +1,43 @@
 import Layout from "../src/layout/Layout"
 import ListadoSkills from "../src/components/ListadoSkills";
 
-const skills = () => {
+const skills = ({frontend, backend, tool}) => {
 
   return (
     <Layout pagina='Skills'>
-      <ListadoSkills/>
+      <ListadoSkills
+        frontend={frontend.data}
+        backend={backend.data}
+        tool={tool.data}
+      />
     </Layout>
   )
+}
+
+export async function getStaticProps() {
+  const urlFrontend = `${process.env.API_URL}/api/frontend?populate=*`;
+  const urlBackend = `${process.env.API_URL}/api/backend?populate=*`;
+  const urlTool = `${process.env.API_URL}/api/tool?populate=*`;
+
+  const [ resFrontends, resBackends, resTool ] = await Promise.all([
+    fetch(urlFrontend),
+    fetch(urlBackend),
+    fetch(urlTool),
+  ]);
+
+  const [ frontend, backend, tool ] = await Promise.all([
+    resFrontends.json(),
+    resBackends.json(),
+    resTool.json(),
+  ]);
+
+  return{
+    props:{
+      frontend,
+      backend,
+      tool,
+    }
+  }
 }
 
 export default skills
