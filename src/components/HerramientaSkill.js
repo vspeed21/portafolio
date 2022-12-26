a63ab8dccf0f5a522c3b19c4df4467c22e8048df
Victor Torres <vtorresquintanilla0206@gmail.com>
2022-12-26 14:09:59 -0600

    change variables according to api request

--- src/components/HerramientaSkill.js
+++ src/components/HerramientaSkill.js
@@ -1,31 +1,27 @@
+import ReactMarkdown from 'react-markdown';
 import Image from 'next/image';
 
-import gitIcon from '../../public/icons/git-color.svg'; 
-import githubIcon from '../../public/icons/github-color.svg';
-
-const HerramientaSkill = () => {
-	const images = [gitIcon, githubIcon]
+const HerramientaSkill = ({ tool }) => {
+	const { images, description } = tool.attributes;
 
   return (
-		<>
-			<ul className='list-disc md:text-lg ml-5'>
-				<h2 className='text-white-dark text-2xl font-bold text-center'>Tools</h2>
-				<li className="mt-5">Git for version control</li>
-				<li>GitHub to collaborate and open source projects</li>
-			</ul>
+    <>
+      <div className='skill'>
+        <ReactMarkdown>{description}</ReactMarkdown>
+      </div>
 
-			<div className='grid grid-cols-2 items-center sm:flex justify-evenly gap-4 py-3 md:py-0 bg-gray-700 shadow-lg'>
-				{images.map(image => (
-					<Image
-						src={image}
-						alt="icon"
-						width={50}
-						height={50}
-					/>
-				))}
-			</div>
-		</>
-	)
+      <div className='grid grid-cols-2 items-center sm:flex justify-evenly gap-4 bg-gray-700 shadow-lg py-3 md:py-0'>
+        {images.data.map(image => (
+          <Image
+            src={image.attributes.url}
+            alt="icon"
+            width={50}
+            height={50}
+          />
+        ))}
+      </div>
+    </>
+  )
 }
 
 export default HerramientaSkill
