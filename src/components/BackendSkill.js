dc7a31b0e09610cdb30af2b29d48a7155f410c83
Victor Torres <vtorresquintanilla0206@gmail.com>
2022-12-26 13:50:11 -0600

    change variables according to api request

--- src/components/BackendSkill.js
+++ src/components/BackendSkill.js
@@ -1,34 +1,27 @@
-import Image from 'next/image'
+import ReactMarkdown from 'react-markdown';
+import Image from 'next/image';
 
-import nodeIcon from '../../public/icons/nodejs-color.svg'; 
-import npmIcon from '../../public/icons/npm-color.svg';
-import dbIcon from '../../public/icons/database.svg'
-
- const BackendSkill = () => {
-	
-  const images = [nodeIcon, npmIcon, dbIcon]
+ const BackendSkill = ({ backend }) => {
+  const { images, description } = backend.attributes;
+  console.log(images);
 
 	return (
 		<>
-			<ul className='list-disc md:text-lg ml-5 md:col-start-2 md:col-end-3'>
-				<h2 className='text-white-dark text-2xl font-bold text-center'>Back End</h2>
-				<li className="mt-5">REST APIs with Node.js & express</li>
-				<li>NPM for package management</li>
-				<li>Mongo DB - mongoose</li>
-				<li>React</li>
-			</ul>
+      <div className='skill md:text-lg ml-5 md:col-start-2 md:col-end-3'>
+        <ReactMarkdown>{description}</ReactMarkdown>
+      </div>
 
-			<div className='grid grid-cols-2 items-center sm:flex justify-evenly gap-4 md:col-start-1 md:col-end-2 md:row-start-2 md:row-end-3 py-3 md:py-0 bg-gray-700 shadow-lg'>
-				{images.map(image => (
-					<Image
-						src={image}
-						alt="icon"
-						width={50}
-						height={50}
-					/>
-				))}
-			</div>
-		</>
+      <div className='grid grid-cols-2 items-center sm:flex justify-evenly gap-4 md:col-start-1 md:col-end-2 md:row-start-2 md:row-end-3 py-3 md:py-0 bg-gray-700 shadow-lg'>
+        {images.data.map(image => (
+          <Image
+            src={image.attributes.url}
+            alt="icon"
+            width={50}
+            height={50}
+          />
+        ))}
+      </div>
+    </>
 	)
 }
 
