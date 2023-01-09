258c8e4d8db2f357225bcee3a1001e873d3479d4
vspeed21 <vtorresquintanilla0206@gmail.com>
2023-01-09 14:40:25 -0600

    Table animations added

--- src/components/Tabla.js
+++ src/components/Tabla.js
@@ -4,17 +4,17 @@ const Tabla = () => {
   return (
     <section className="bg-secondary mt-16 py-12">
       <div className=" contenedor flex flex-col md:flex-row justify-evenly items-center text-white gap-9 md:gap-0">
-        <div className="text-center">
+        <div className="text-center" data-aos="zoom-out-down" data-aos-duration="1000">
           <p className="text-5xl md:text-6xl mb-2 font-bold">2+</p>
           <p className="text-xl first-letter:uppercase">years of experiencie</p>
         </div>
 
-        <div className="text-center">
+        <div className="text-center" data-aos="zoom-in" data-aos-duration="1000">
           <p className="text-5xl md:text-6xl mb-2 font-bold">50+</p>
           <p className="text-xl first-letter:uppercase">projects</p>
         </div>
 
-        <div className="text-center">
+        <div className="text-center" data-aos="zoom-in-up" data-aos-duration="1000">
           <p className="text-5xl md:text-6xl mb-2 font-bold">20+</p>
           <p className="text-xl first-letter:uppercase">skills</p>
         </div>
