9696114781ca6a2e3cb4c523089c3a21058bc595
vspeed21 <vtorresquintanilla0206@gmail.com>
2023-01-09 20:46:37 -0600

    ToolSkill animations added

--- src/components/HerramientaSkill.js
+++ src/components/HerramientaSkill.js
@@ -6,13 +6,14 @@ const HerramientaSkill = ({ tool }) => {
 
   return (
     <>
-      <div className="skill md:text-lg ml-5">
+      <div className="skill md:text-lg ml-5" data-aos="zoom-in">
         <ReactMarkdown>{description}</ReactMarkdown>
       </div>
 
       <div className='grid grid-cols-2 items-center sm:flex justify-evenly gap-4 bg-gray-700 shadow-lg py-3 md:py-0'>
         {images?.data.map(image => (
           <Image
+            data-aos={image.id % 2 ? 'fade-down' : 'fade-up'}
             key={image?.id}
             src={image?.attributes?.url}
             alt="icon"
