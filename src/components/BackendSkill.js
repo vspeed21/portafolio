d0d7a36a0f3924b219e2c64df667a37ca1300d07
vspeed21 <vtorresquintanilla0206@gmail.com>
2023-01-09 20:42:48 -0600

    BackendSkill animations added

--- src/components/BackendSkill.js
+++ src/components/BackendSkill.js
@@ -6,13 +6,14 @@ import Image from 'next/image';
 
 	return (
 		<>
-      <div className='skill md:text-lg ml-5 md:col-start-2 md:col-end-3'>
+      <div className='skill md:text-lg ml-5 md:col-start-2 md:col-end-3' data-aos="zoom-in">
         <ReactMarkdown>{description}</ReactMarkdown>
       </div>
 
       <div className='grid grid-cols-2 items-center sm:flex justify-evenly gap-4 md:col-start-1 md:col-end-2 md:row-start-2 md:row-end-3 py-3 md:py-0 bg-gray-700 shadow-lg'>
         {images?.data.map(image => (
           <Image
+            data-aos={image.id % 2 ? 'fade-down' : 'fade-up'}
             key={image.id}
             src={image?.attributes?.url}
             alt="icon"
